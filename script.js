8535a495c689acce9aae7bf913de5bed98a2394e
DBENTLEY <david.bentley95@gmail.com>
2024-11-16 13:58:44 -0800

    Change board size selector to input range
    Use input range slider to select the size of board instead of
    asking for user input or using preset buttons (previous method).
    
    Starting with min and max of 4 - 25 because larger numbers crash.

--- script.js
+++ script.js
@@ -8,9 +8,8 @@ function setBoardSize(event) {
     for(let i=1; i<=numOfSquares*numOfSquares; i++) {
         drawingBoard.innerHTML += `<div class="pixel"></div>`;  
     }
-    
 }
 
-document.querySelector("#board-size-buttons").addEventListener("click", setBoardSize);
+document.querySelector("#board-size-selector").addEventListener("input", setBoardSize);
 
 document.querySelector("#drawing-board").addEventListener("mouseover", (e) => e.target.style.backgroundColor = "black");
