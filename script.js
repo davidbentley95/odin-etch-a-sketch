d1056b7f10cc6109fe147caed48df6da6b619d84
DBENTLEY <david.bentley95@gmail.com>
2024-11-16 07:40:30 -0800

    Set up logic for setting drawing board squares
    Create buttons to store value of the dimension of the drawing board.
    Create event listeners to listen for click.
    Create event function to retrieve value of button and loop
    to create divs corresponding to dimension.
    Include in event function dynamic changing of CSS to accommodate
    updates to dimensions.

---
+++ script.js
@@ -0,0 +1,14 @@
+const drawingBoard = document.querySelector("#drawing-board");
+
+function setBoardSize(event) {
+    let numOfSquares = event.target.value;
+    drawingBoard.innerHTML = "";
+    drawingBoard.style.gridTemplateColumns = `repeat(${numOfSquares}, 1fr)`;
+    drawingBoard.style.gridTemplateRows = `repeat(${numOfSquares}, 1fr)`;
+    for(let i=1; i<=numOfSquares*numOfSquares; i++) {
+        drawingBoard.innerHTML += `<div class="pixel"></div>`;  
+    }
+    
+}
+
+document.querySelector("#board-size-buttons").addEventListener("click", setBoardSize);
