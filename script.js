3897d00b26746906f4b2375987832367c7fdf972
DBENTLEY <david.bentley95@gmail.com>
2024-11-19 06:12:32 -0800

    Clean up files before starting styling
    Remove unnecessary comments and redundant values.

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-const PASTEL_COLOR_PALETTE = ["#223030", "#523D35", "#959D90", "#BBA58F", "#E8D9CD", "#EFEFE9"];
+const EARTHY_COLOR_PALETTE = ["#223030", "#523D35", "#959D90", "#BBA58F", "#E8D9CD", "#EFEFE9"];
 const drawingBoard = document.querySelector("#drawing-board");
 const rangeBar = document.querySelector("#board-size-selector");
 
 let numOfSquares = rangeBar.value;
 let pixelColor = "black";
 let randomColorFlag = false;
-
-let lastExecution = 0;
-let lastPosition = {x: 0, y: 0};
-let currentPosition = {x: 0, y: 0};
-let lastTime = 0;
-
 
 // Set default board on load
 
@@ -21,8 +15,8 @@ for(let i=1; i<=numOfSquares*numOfSquares; i++) {
 }
 
 function randomColorPicker() {
-    let randomIndex = Math.floor(Math.random() * (PASTEL_COLOR_PALETTE.length -1));
-    pixelColor = PASTEL_COLOR_PALETTE[randomIndex];
+    let randomIndex = Math.floor(Math.random() * (EARTHY_COLOR_PALETTE.length -1));
+    pixelColor = EARTHY_COLOR_PALETTE[randomIndex];
 }
 
 function setPixelColor(pixel) {
@@ -45,29 +39,19 @@ function setBoardSize(event) {
 
 // event handle function for darkening squares
 function darkenSquare(event) {
+    if(event.target.classList.contains("pixel")){
 
-    console.log(`Original Last position ${Object.values(lastPosition)}`);
-    console.log(`Original Current position ${Object.values(currentPosition)}`);
-
-    let x = event.clientX;
-    let y = event.clientY;
-
-    currentPosition = {x, y};
-    console.log(`Updated current position ${Object.values(currentPosition)}`);
-    let currentOpacity = Number(window.getComputedStyle(event.target).getPropertyValue("opacity"));
-    if(randomColorFlag && currentOpacity === 0) {
-        randomColorPicker();
-        setPixelColor(event.target);
-    }
-    event.target.style.opacity = currentOpacity + 0.1;
-    lastPosition= {x, y};
-    console.log(`Updated Last position ${Object.values(lastPosition)}`);
+        let currentOpacity = Number(window.getComputedStyle(event.target).getPropertyValue("opacity"));
+        if(randomColorFlag && currentOpacity === 0) {
+            randomColorPicker();
+            setPixelColor(event.target);
+        }
+        event.target.style.opacity = currentOpacity + 0.1;}
 };
 
+//Event Listeners
 document.querySelector("#board-size-selector").addEventListener("input", setBoardSize);
-
 document.querySelector("#drawing-board").addEventListener("mouseover", darkenSquare);
-
 document.querySelector(".black-button").addEventListener("click", () => randomColorFlag = false);
-
 document.querySelector(".earthy-button").addEventListener("click", () => randomColorFlag = true);
+
