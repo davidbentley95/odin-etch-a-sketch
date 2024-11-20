148148b0de86e88ef306ca2c929c7afd510c0ef0
DBENTLEY <david.bentley95@gmail.com>
2024-11-20 09:49:09 -0800

    WIP - debug mobile features
    Implement tracking of cursor location which returns
    true if in current div based on the coordinates of the div.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let numOfSquares = rangeBar.value;
 let pixelColor = "black";
 let randomColorFlag = false;
 let pixelNumber = 1;
+let currentDiv = null;
 
 // Set default board on load
 for(let i=1; i<=numOfSquares*numOfSquares; i++) {
@@ -37,7 +38,15 @@ function roundCornerPixels() {
     pixels[boardSize-1].style.borderRadius = "0 0 20px 0";
 }
 
-// event handle function for changing slider
+// function setPixelCoordinates() {
+//     const pixels = document.querySelectorAll(".pixel");
+//     pixels.forEach((cell) => {
+//         const rect = cell.getBoundingClientRect();
+//         console.log(`x=${rect.x}, y=${rect.y}`);
+//     })
+// }
+
+// event handler function for changing slider
 function setBoardSize(event) {
     numOfSquares = event.target.value;
     drawingBoard.innerHTML = "";
@@ -54,7 +63,7 @@ function setBoardSize(event) {
     roundCornerPixels();
 };
 
-// event handle function for darkening squares
+// event handler function for darkening squares
 function darkenSquare(event) {
     if(event.target.classList.contains("pixel")){
 
@@ -66,9 +75,47 @@ function darkenSquare(event) {
         event.target.style.opacity = currentOpacity + 0.1;}
 };
 
+//event handler function for touch movement
+function handleTouch(event) {
+    const touch = event.touches[0];
+    const x = touch.clientX;
+    const y = touch.clientY;
+    const touchedElement = document.elementFromPoint(x,y);
+
+};
+
 //Event Listeners
 document.querySelector("#board-size-selector").addEventListener("input", setBoardSize);
 document.querySelector("#drawing-board").addEventListener("mouseover", darkenSquare);
 document.querySelector(".black-button").addEventListener("click", () => randomColorFlag = false);
 document.querySelector(".rgb-button").addEventListener("click", () => randomColorFlag = true);
+
+// document.querySelector("#drawing-board").addEventListener("touchmove", e => {console.log(e.touches);
+//     console.log(e.target.getBoundingClientRect());
+//     console.log(`X: ${e.touches[0].clientX}`);
+//     console.log(`Y: ${e.touches[0].clientY}`);
+// });
+
+document.querySelector("#drawing-board").addEventListener("touchmove", trackTouchMovement);
+document.querySelector("#drawing-board").addEventListener("touchstart", trackTouchMovement);
+
+
+function isWithinDiv(x, y, rect) {
+    return x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom;
+};
+
+function trackTouchMovement(event) {
+    
+    const touchX = event.touches[0].clientX;
+    console.log(touchX);
+    const touchY = event.touches[0].clientY;
+    console.log(touchY);
+    currentDiv = event.target;
+    console.log(currentDiv);
+
+    console.log((isWithinDiv(touchX, touchY, currentDiv.getBoundingClientRect())));
+    if(isWithinDiv(touchX, touchY, currentDiv.getBoundingClientRect())) {
+        currentDiv.style.backgroundColor = "yellow"
+    }
+};
 
